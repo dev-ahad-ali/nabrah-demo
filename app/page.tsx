c74473c60d1030ba725de0846c57ffb5fc3b638b
dev-ahad-ali <ahadali.dev@gmail.com>
2026-02-02 18:13:08 +0600

    Update: marque component created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Client } from "@/components/home/Clients";
 import Hero from "@/components/home/Hero";
 
 export default function Home() {
     return (
         <>
             <Hero />
+            <Client />
         </>
     );
 }
--- components/home/Clients.tsx
+++ components/home/Clients.tsx
@@ -1,3 +1,66 @@
-export default function Clients() {
-    return <h2>some</h2>;
+"use client";
+
+import { Marque } from "../shared/Marque";
+
+const testimonials = [
+    {
+        quote: "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+        name: "Charles Dickens",
+        title: "A Tale of Two Cities",
+    },
+    {
+        quote: "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+        name: "William Shakespeare",
+        title: "Hamlet",
+    },
+    {
+        quote: "All that we see or seem is but a dream within a dream.",
+        name: "Edgar Allan Poe",
+        title: "A Dream Within a Dream",
+    },
+    {
+        quote: "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+        name: "Jane Austen",
+        title: "Pride and Prejudice",
+    },
+    {
+        quote: "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+        name: "Herman Melville",
+        title: "Moby-Dick",
+    },
+];
+
+export function Client() {
+    return (
+        <div className="rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+            <Marque direction="right" speed="slow">
+                {testimonials.map((item, idx) => (
+                    <div
+                        className="relative w-[350px] max-w-full shrink-0 rounded-2xl border border-b-0 border-zinc-200 bg-[linear-gradient(180deg,#fafafa,#f5f5f5)] px-8 py-6 md:w-[450px] dark:border-zinc-700 dark:bg-[linear-gradient(180deg,#27272a,#18181b)]"
+                        key={item.name}
+                    >
+                        <blockquote>
+                            <div
+                                aria-hidden="true"
+                                className="user-select-none pointer-events-none absolute -top-0.5 -left-0.5 -z-1 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
+                            ></div>
+                            <span className="relative z-20 text-sm leading-[1.6] font-normal text-neutral-800 dark:text-gray-100">
+                                {item.quote}
+                            </span>
+                            <div className="relative z-20 mt-6 flex flex-row items-center">
+                                <span className="flex flex-col gap-1">
+                                    <span className="text-sm leading-[1.6] font-normal text-neutral-500 dark:text-gray-400">
+                                        {item.name}
+                                    </span>
+                                    <span className="text-sm leading-[1.6] font-normal text-neutral-500 dark:text-gray-400">
+                                        {item.title}
+                                    </span>
+                                </span>
+                            </div>
+                        </blockquote>
+                    </div>
+                ))}
+            </Marque>
+        </div>
+    );
 }
